4c563a559d98e3ab17915fc17bdf961e2c4dfd47
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-04-14 03:06:02 +0500

    sdfdsfsd

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const hostname = '127.0.0.1';
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get('/api/numbers', (req, res) => {
+    res.send([1,3,4,5])
+})
+
+app.get('/api/chars', (req, res) => {
+    res.send(['a', 'b', 'c', 'd', 'e', 'f'])
+})
+
+app.get('/api/cars', (req, res) => {
+    res.send(['BMD', 'Carrolla', 'Caltus', 'Suzuki', 'MiniBus', 'Datson'])
+})
+
+// to set environment variable use set PORT=5000 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listining To Port ${port}...`) )
